fba27151769195c39b29a5bfa8dae7e2eb88f5bd
Jihoon Park <analysispark@gmail.com>
2023-01-06 14:37:57 +0900

    third commit

--- main/urls.py
+++ main/urls.py
@@ -5,9 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='main_index'),
     path('index', views.index, name='main_index'),
-    path('classes', views.classes, name='main_classes'),
-    path('contact', views.contact, name='main_contact'),
-    path('shortcodes', views.shortcodes, name='main_shortcodes'),
-    path('shows', views.shows, name='main_shows'),
     path('about', views.about, name='main_about'),
+    path('calculator', views.calculator, name='main_calculator'),
+    path('result', views.result, name='main_result'),
+    
 ]
--- main/views.py
+++ main/views.py
@@ -4,17 +4,11 @@ from django.shortcuts import render, redirect
 def index(request):
     return render(request, 'main/index.html')
 
-def classes(request):
-    return render(request, 'main/classes.html')
-
-def contact(request):
-    return render(request, 'main/contact.html')
-
-def shortcodes(request):
-    return render(request, 'main/shortcodes.html')
-
-def shows(request):
-    return render(request, 'main/shows.html')
-
 def about(request):
     return render(request, 'main/about.html')
+
+def result(request):
+    return render(request, 'main/result.html')
+
+def calculator(request):
+    return render(request, 'main/calculator.html')
