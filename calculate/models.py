b22722208c0e0377a5a42dad57ecfbdaf5105cfa
analysispark <analysispark@gmail.com>
2023-01-18 21:20:02 +0900

    sql 데이터불러오기 성공

--- calculate/models.py
+++ calculate/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class TEST (models.Model):
 
     Match = models.CharField(max_length=255, null=True)
-    Game = models.IntegerField(null=True)
+    Game = models.CharField(max_length=10, null=True)
     Team = models.CharField(max_length=150, null=True)
     Name = models.CharField(max_length=150, null=True)
-    v_2p = models.IntegerField(null=True)
+    v_2p = models.CharField(max_length=10, null=True, default='')
