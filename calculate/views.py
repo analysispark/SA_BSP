965a81355e0ab42bc04917cc886daf7dd554977a
Jihoon, Park <analysispark@gmail.com>
2023-02-02 22:46:10 +0900

    2023_02_02

--- calculate/views.py
+++ calculate/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+import pandas as pd
 
 # Create your views here.
+
+def main_view(request):
+    qs = calculate_test.objects.all()
+    context = {'qs': qs}
+    return render(request, '')
--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+import pandas as pd
 
 # Create your views here.
 def index(request):
