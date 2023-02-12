1f9d72bf19f6e1ca7e06b07f0b9bd32cf1384a37
Jihoon, Park <analysispark@gmail.com>
2023-02-12 22:54:16 +0900

    2023_02_02

--- calculate/views.py
+++ calculate/views.py
@@ -3,7 +3,19 @@ import pandas as pd
 
 # Create your views here.
 
-def main_view(request):
+class db_calc(request):
     qs = calculate_test.objects.all()
-    context = {'qs': qs}
-    return render(request, '')
+    
+    def main_view(request):     # Select
+        df = pd.DataFrame(qs)
+        context = {'df': df}
+        return render(request, '')
+
+
+    def KPI_gen()               # KPI generator of Selection
+        pass
+
+    def KPI_cal(models.Model):
+        KPI_Atk = pass
+        KPI_Def = pass
+        KPI_Per = pass
--- result/models.py
+++ result/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import pandas as pd
 
 # Create your models here.
+def certi_result():
+    pass
+
+
+def cert():
+    import docx
+    from docx.oxml.ns import qn
+    from docx.enum.text import WD_ALIGN_PARAGRAPH
+    from docx2pdf import convert
+    from datetime import datetime
